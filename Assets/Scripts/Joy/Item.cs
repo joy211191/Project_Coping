6195357ab17ed43a70ef2d03470bddc4758d7a1f
Joy Goldar <joy211191@gmail.com>
2020-04-23 11:20:12 +0200

    Revert "CS Update"
    This reverts commit aad4c41d1cf7c8643f2af753d25397d429833484.

---
+++ Assets/Scripts/Joy/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum ItemType {
+    Portions,
+    Equipment,
+    WillTokens,
+    RepairKit
+}
+
+
+[CreateAssetMenu(fileName = "Item", menuName = "InventoryItems", order = 1)]
+public class Item : ScriptableObject
+{
+    public ItemType itemType;
+    public string nameOfItem;
+    //public float 
+}
