06fda065e8720ae2beaea43e5a9a0e5a84af000c
Bengan <58001599+Ben-484@users.noreply.github.com>
2020-04-28 12:55:33 +0200

    Added comments to Level Generation script

--- Assets/Scripts/Bengt/Level Generation/LevelGenerationManager.cs
+++ Assets/Scripts/Bengt/Level Generation/LevelGenerationManager.cs
@@ -10,34 +10,24 @@ public class LevelGenerationManager : MonoBehaviour
     int                 m_sectionLength; //Length of a single section
     [SerializeField]
     List<GameObject>    m_RoomPrefabsList       = new List<GameObject>(); //Room Class? Store roomtype?
-
-    protected int       m_sectionNumber         = 1;
-
-
-    List<GameObject>    m_CurrentSectionRooms   = new List<GameObject>(); //Rooms in the current section
-    List<GameObject>    m_NextSectionRooms      = new List<GameObject>(); //Rooms in the next section
-
     [SerializeField]
     GameObject m_StartRoom;
     [SerializeField]
     GameObject m_EndRoom;
 
+    protected int       m_sectionNumber         = 1;
 
+    List<GameObject>    m_CurrentSectionRooms   = new List<GameObject>(); //Rooms in the current section
+    List<GameObject>    m_NextSectionRooms      = new List<GameObject>(); //Rooms in the next section
 
     //TODO: Some way to increase rooms in future sections?
     //Exponential curve? Some other type of curve?
     //make sure that we only get certain amaounts of certain types of rooms?
-
-
-    //int m_nextsectionLength; ?
-    //Rooms in Section
-    //Rooms in next Section
-    //Rooms per section?
-
-
+    //Have the player "teleport" to the next section when reaching the end
 
     void Awake()
     {
+        //Create Current and next section
         GenerateSection(m_sectionLength, m_RoomPrefabsList, m_CurrentSectionRooms);
         GenerateSection(m_sectionLength, m_RoomPrefabsList, m_NextSectionRooms);
     }
@@ -48,16 +38,10 @@ public class LevelGenerationManager : MonoBehaviour
         
     }
 
-
     //Generates a section of rooms
     private void GenerateSection(int p_inLength, List<GameObject> p_inList, List<GameObject> p_inListDest)
     {
-        //Add Start room
-        //p_inListDest.Add(Instantiate(m_StartRoom, new Vector2(-m_StartRoom.transform.Find("Sprites").GetComponent<SpriteRenderer>().size.x, 50), Quaternion.identity));
-
         float m_tempXdif = 0;
-
-        
 
         for (int i = 0; i <= p_inLength + 2; i++)
         {
@@ -65,43 +49,28 @@ public class LevelGenerationManager : MonoBehaviour
             float prevRoomYDiff = 0;
             float totalRoomYDiff = 0;
 
-            if (i == 0)
+            if (i == 0)//If first room, instantiate start room
                 p_inListDest.Add(Instantiate(m_StartRoom, new Vector2(i * m_tempXdif, m_sectionNumber * 100), Quaternion.identity));
-            else if (i == p_inLength + 2)
+            else if (i == p_inLength + 2) //If last room, instantiate end room
                 p_inListDest.Add(Instantiate(m_EndRoom, new Vector2(i * m_tempXdif, m_sectionNumber * 100), Quaternion.identity));
-            else
+            else //if neither, instantiate a random non-start non-end room
                 p_inListDest.Add(Instantiate(p_inList[RandomNumber(0, m_RoomPrefabsList.Count - 1)], new Vector2(i * m_tempXdif, m_sectionNumber * 100), Quaternion.identity));
-
 
             if (i > 0)
             {
-                currRoomYDiff -= p_inListDest[i].transform.Find("Entrance").transform.position.y;
-                prevRoomYDiff += p_inListDest[i - 1].transform.Find("Exit").transform.position.y;
-                totalRoomYDiff = prevRoomYDiff + currRoomYDiff;
+                currRoomYDiff -= p_inListDest[i].transform.Find("Entrance").transform.position.y; //Find difference in Y position between Entrance and center of the current room and invert
+                prevRoomYDiff += p_inListDest[i - 1].transform.Find("Exit").transform.position.y; //Find difference in Y position between Exit and center of the previous room
+                totalRoomYDiff = prevRoomYDiff + currRoomYDiff; //add both together
             }
 
-            p_inListDest[i].transform.position = new Vector3(p_inListDest[i].transform.position.x, p_inListDest[i].transform.position.y + totalRoomYDiff, p_inListDest[i].transform.position.z); 
-            m_tempXdif = p_inListDest[i].transform.Find("Sprites").GetComponent<SpriteRenderer>().size.x;
+            p_inListDest[i].transform.position = new Vector3(p_inListDest[i].transform.position.x, p_inListDest[i].transform.position.y + totalRoomYDiff, p_inListDest[i].transform.position.z); //Change room position depending on RoomYDiff
+            m_tempXdif = p_inListDest[i].transform.Find("Sprites").GetComponent<SpriteRenderer>().size.x; //Change m_tempXdif to width of the newly insatntiated room
 
         }
-        m_sectionNumber++;
 
-        //currRoomYDiff -= p_inListDest[p_inListDest.Count - 1].transform.Find("Entrance").transform.position.y;
-        //prevRoomYDiff += p_inListDest[p_inListDest.Count - 2].transform.Find("Exit").transform.position.y;
+        m_sectionNumber++; //Increase section number after instantiating a full section
 
-        //p_inListDest.Add(Instantiate(m_EndRoom, new Vector2(p_inListDest.Count * m_tempXdif, 50), Quaternion.identity));
-        //p_inListDest[p_inListDest.Count - 1].transform.position = new Vector3(p_inListDest[p_inListDest.Count - 1].transform.position.x, p_inListDest[p_inListDest.Count - 1].transform.position.y + totalRoomYDiff, p_inListDest[p_inListDest.Count - 1].transform.position.z);
-    }
-
-
-    //Old Funtion that did'nt work properly
-    //private int RandRoom()
-    //{
-    //    System.Random m_rand = new System.Random();
-
-
-    //    return m_rand.Next(0, m_RoomPrefabsList.Count - 1);
-    //}
+       }
 
     //Function to get a random number 
     private static readonly System.Random random = new System.Random();
@@ -109,7 +78,7 @@ public class LevelGenerationManager : MonoBehaviour
     public static int RandomNumber(int min, int max)
     {
         lock (syncLock)
-        { // synchronize
+        {
             return random.Next(min, max);
         }
     }
