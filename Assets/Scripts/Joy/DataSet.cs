e8658437f21969e63c306a6bc705d166ea67b985
Joy Goldar <joy211191@gmail.com>
2020-04-29 14:53:43 +0200

    Graph visualization
    Finally!!!

---
+++ Assets/Scripts/Joy/DataSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataSet {
+    public int selfSoothing;
+    public int numbing;
+    public int compulsiveRiskTaking;
+    public int selfHarm;
+    public int escape;
+    public int livesUsed;
+}
